d0745d34a50b2c7384556ac6c64374a4b8941f40
jsteenb2 <jonathansteenbergen@gmail.com>
2016-02-29 22:41:24 +0000

    html file being sent now instead of text

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var fs = require('fs');
+var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.sendfile('index.html');
 })
 
 app.listen(app.get('port'), function() {
