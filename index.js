00daf07f9abd911421b86dea55ecc14aff81ebd5
jsteenb2 <jonathansteenbergen@gmail.com>
2016-02-29 23:10:38 +0000

    Fixed typo in fn to serve html file

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.sendfile(__dirname +'index.html');
+  response.sendfile('index.html');
 })
 
 app.listen(app.get('port'), function() {
